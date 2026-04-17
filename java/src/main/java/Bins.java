784c22acec190041b2b3013626fa7d5061ec2d76
thejoeflynn <thejoeflynn@gmail.com>
2026-04-16 20:18:22 -0400

    Setup Dice.class

--- java/src/main/java/Bins.java
+++ java/src/main/java/Bins.java
@@ -1,4 +1,6 @@
 
 public class Bins {
-
+Bins results = new Bins(2, 12); // for bins from 2..12
+Integer numberOfTens = results.getBin(10); // returns the number of tens in the 10 bin
+results.incrementBin(10); // should increment bin # 10
 }
--- java/src/main/java/Dice.java
+++ java/src/main/java/Dice.java
@@ -1,4 +1,37 @@
+
+import java.util.Random;
+
 public class Dice {
 
+    private int numberOfDice;
 
+    public Dice(int numberOfDice) {
+        this.numberOfDice = numberOfDice;
+    }
+
+    public int tossAndSum() {
+        Random random = new Random();
+        int sum = 0;
+        for (int i = 0; i < numberOfDice; i++) {
+            sum += random.nextInt(6) + 1;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Dice dice = new Dice(5); // for yatzee
+        Dice dice1 = new Dice(2); // for craps
+
+        Integer toss = dice.tossAndSum();
+        Integer toss1 = dice1.tossAndSum();
+
+        System.out.println("Toss and sum of 5 dice: " + toss);
+        System.out.println("Toss and sum of 2 dice: " + toss1);
+    }
+    
+
+
+
+
+    
 }
