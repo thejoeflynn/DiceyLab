58e0f9323326c1ba772290f0396360d8eb090bc5
thejoeflynn <thejoeflynn@gmail.com>
2026-04-17 08:30:18 -0400

    setup Bins

--- java/src/main/java/Bins.java
+++ java/src/main/java/Bins.java
@@ -1,7 +1,26 @@
 
 public class Bins {
-Bins results = new Bins(2, 12); // for bins from 2..12
-Integer numberOfTens = results.getBin(10); // returns the number of tens in the 10 bin
-results.incrementBin(10); // should increment bin # 10
+    private int min;
+    private int max;
+    private int[] bins;
+
+    public Bins(int min, int max) {
+        this.min = min;
+        this.max = max;
+        this.bins = new int[max - min +1];
+    }
+
+    public int getBin(int number) {
+        return bins[number-min];
+    }
+
+    public void incrementBin(int number) {
+        bins[number - min]++;
+    }
+
+    
+
+
+
 
 }
